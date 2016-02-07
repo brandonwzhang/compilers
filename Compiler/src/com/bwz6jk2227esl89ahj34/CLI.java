ee6d09b53be2ed55f234f4517477647ad3d9e76c
Brandon Zhang <bwzhang@gmail.com>
2016-02-06 22:29:34 -0500

    Changed option stuff

--- Compiler/src/com/bwz6jk2227esl89ahj34/CLI.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/CLI.java
@@ -2,31 +2,48 @@ package com.bwz6jk2227esl89ahj34;
 
 import java.util.Map;
 import java.util.HashMap;
+
 /**
  * Created by jihunkim on 2/6/16.
  */
 public class CLI {
-    public static Map<String, Option> options;
+    public Map<String, Option> options;
 
     public CLI() {
         options = new HashMap<>();
+        Option.Action helpAction = args -> printOptions();
+        Option helpOption = new Option("List commands", helpAction);
+        options.put("--help", helpOption);
     }
 
-    public static void execute(String[] args) {
-        if(args.length == 0) {
-            options.get("--help").action(args);
+    public void execute(String[] args) {
+        if (args.length == 0) {
+            options.get("--help").action.run(args);
         } else {
-          options.get(args[0]).action(args);
+            options.get(args[0]).action.run(args);
         }
     }
 
-    public static void printOptions() {
-        for(String option : options.keySet()) {
-            System.out.println(option + "\t\t"+ options.get(option).getDescription());
+    public void printOptions() {
+        for (String option : options.keySet()) {
+            System.out.println(option + "\t\t" +
+                    options.get(option).description);
         }
     }
 
+    public void addOption(String optionName,
+                          String description,
+                          Option.Action action) {
+        Option option = new Option(description, action);
+        options.put(optionName, option);
+    }
 
-
-
+    public void addOption(String[] optionNames,
+                          String description,
+                          Option.Action action) {
+        Option option = new Option(description, action);
+        for (String optionName : optionNames) {
+            options.put(optionName, option);
+        }
+    }
 }
