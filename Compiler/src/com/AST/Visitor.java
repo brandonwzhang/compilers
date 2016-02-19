b297aff6d02fd9a09537a1d928a0ba6ea33baff2
Eric Lin <ericsuhelin@gmail.com>
2016-02-18 23:18:33 -0500

    added stubs for astnode abstract class and visitor class

---
+++ Compiler/src/com/AST/Visitor.java
@@ -0,0 +1,22 @@
+package com.AST;
+
+/**
+ * Created by eric on 2/18/16.
+ */
+public class Visitor {
+    public Visitor(){
+
+    }
+
+    public void preVisit(ASTNode node){
+
+    }
+
+    public void visit(ASTNode node){
+
+    }
+
+    public void postVisit(ASTNode node){
+
+    }
+}
