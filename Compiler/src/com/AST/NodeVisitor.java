b2e54613321925cf9973606f2328351e6c4dfccd
Brandon Zhang <bwzhang@gmail.com>
2016-02-18 23:23:43 -0500

    Changed Visitor to NodeVisitor

---
+++ Compiler/src/com/AST/NodeVisitor.java
@@ -0,0 +1,22 @@
+package com.AST;
+
+/**
+ * Created by eric on 2/18/16.
+ */
+public class NodeVisitor {
+    public NodeVisitor(){
+
+    }
+
+    public void preVisit(Node node){
+
+    }
+
+    public void visit(Node node){
+
+    }
+
+    public void postVisit(Node node){
+
+    }
+}
