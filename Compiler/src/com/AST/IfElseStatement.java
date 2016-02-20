b1766b47b88779724f46a620a4392a70c88cef5a
Ji Hun Kim <jk2227@cornell.edu>
2016-02-19 23:40:03 -0500

    Merged back IfStatement with IfElseStatement

--- Compiler/src/com/AST/IfElseStatement.java
+++
@@ -1,9 +0,0 @@
-package com.AST;
-
-import lombok.Data;
-
-public @Data class IfElseStatement extends Statement {
-    private Expression guard;
-    private Block trueBlock;
-    private Block falseBlock;
-}
--- Compiler/src/com/AST/IfStatement.java
+++ Compiler/src/com/AST/IfStatement.java
@@ -2,7 +2,10 @@ package com.AST;
 
 import lombok.Data;
 
+import java.util.Optional;
+
 public @Data class IfStatement extends Statement {
     private Expression guard;
-    private Block block;
+    private Block trueBlock;
+    private Optional<Block> falseBlock;
 }
