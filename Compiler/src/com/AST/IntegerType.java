b703474d917ace5d8506af5a4aab24bcbaac7baf
Eric Lin <ericsuhelin@gmail.com>
2016-02-20 07:07:59 -0500

    added integertype

---
+++ Compiler/src/com/AST/IntegerType.java
@@ -0,0 +1,12 @@
+package com.AST;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class IntegerType extends Type{
+
+}
