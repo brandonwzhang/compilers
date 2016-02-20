b96e3e08cd7e608aa0ada7902fb13857554dfcd8
Ji Hun Kim <jk2227@cornell.edu>
2016-02-20 00:51:18 -0500

    lol

--- Compiler/src/com/AST/FunctionCall.java
+++ Compiler/src/com/AST/FunctionCall.java
@@ -1,9 +1,11 @@
 package com.AST;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.util.List;
-
-public @Data class FunctionCall extends Expression {
+@AllArgsConstructor
+@Data
+public class FunctionCall extends Expression {
     private Identifier identifier;
     private List<Expression> arguments;
 }
