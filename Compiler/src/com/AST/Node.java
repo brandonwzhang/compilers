eef39562ba1cb95ad45e57b3d04ca05e3deedc0d
Andy Jiang <ahj34@cornell.edu>
2016-02-20 01:59:59 -0500

    removed preVisit and postVisit

--- Compiler/src/com/AST/Node.java
+++ Compiler/src/com/AST/Node.java
@@ -3,8 +3,6 @@ package com.AST;
 public abstract class Node {
 
     public void accept(NodeVisitor v){
-        v.preVisit(this);
         v.visit(this);
-        v.postVisit(this);
     }
 }
--- Compiler/src/com/AST/NodeVisitor.java
+++ Compiler/src/com/AST/NodeVisitor.java
@@ -5,15 +5,7 @@ public class NodeVisitor {
 
     }
 
-    public void preVisit(Node node){
-
-    }
-
     public void visit(Node node){
-
-    }
-
-    public void postVisit(Node node){
 
     }
 }
