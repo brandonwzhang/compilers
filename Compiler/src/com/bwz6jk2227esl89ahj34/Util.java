9316e67de11ff43ec0a9f1025d27efde9f962a95
Andy Jiang <ahj34@cornell.edu>
2016-02-20 16:51:40 -0500

    added a Util function for comparing s expressions

--- Compiler/src/com/bwz6jk2227esl89ahj34/Util.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/Util.java
@@ -1,6 +1,11 @@
 package com.bwz6jk2227esl89ahj34;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Util {
@@ -16,5 +21,26 @@ public class Util {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static boolean compareSExp(String fileName1, String fileName2) throws IOException {
+
+        String sExp1 = new String(Files.readAllBytes(Paths.get(fileName1)),
+                Charset.forName("UTF-8"));
+        String sExp2 = new String(Files.readAllBytes(Paths.get(fileName2)),
+                Charset.forName("UTF-8"));
+
+        sExp1 = sExp1.replaceAll("\\s+", " ")
+                .replaceAll("\\(\\s?", "(")
+                .replaceAll("\\s?\\)", ")");
+
+        sExp2 = sExp2.replaceAll("\\s+", " ")
+                .replaceAll("\\(\\s?", "(")
+                .replaceAll("\\s?\\)", ")");
+
+        System.out.println(sExp1);
+        System.out.println(sExp2);
+
+        return sExp1.equals(sExp2);
     }
 }
