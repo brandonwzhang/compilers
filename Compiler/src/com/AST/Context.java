faa6d16602cd8dfe70fc7a98c94d3b5f0acc17c1
Andy Jiang <ahj34@cornell.edu>
2016-02-28 16:34:48 -0500

    added constructors to Context

--- Compiler/src/com/AST/Context.java
+++ Compiler/src/com/AST/Context.java
@@ -4,4 +4,11 @@ import java.util.HashMap;
 
 public class Context extends HashMap<Identifier, Type> {
 
+    public Context() {
+        super();
+    }
+
+    public Context(Context c) {
+        super(c);
+    }
 }
