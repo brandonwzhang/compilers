867b6444ff3b4c50778e20e68d073cddf8d458fd
Eric Lin <ericsuhelin@gmail.com>
2016-02-29 17:05:51 -0500

    changed typeexception class

--- Compiler/src/com/bwz6jk2227esl89ahj34/TypeException.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/TypeException.java
@@ -1,7 +1,25 @@
 package com.bwz6jk2227esl89ahj34;
 
 public class TypeException extends RuntimeException {
+    private int row;
+    private int col;
+    private String errMessage;
+
     public TypeException(String message) {
         super(message);
+    }
+
+    public TypeException(String message, int row, int col) {
+        this.errMessage = message;
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public void printStackTrace() {
+        System.out.print("Semantic error: beginning at ");
+        System.out.print(row + ":");
+        System.out.print(col + ": ");
+        System.out.println(this.errMessage);
     }
 }
