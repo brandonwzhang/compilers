89071c6c3751c4aeb4491b4dfa9786acdc4fecab
Brandon Zhang <bwzhang@gmail.com>
2016-04-06 00:17:56 -0400

    Added constructors for AssemblyMemoryLocation

--- Compiler/src/com/bwz6jk2227esl89ahj34/AST/code_generation/AssemblyMemoryLocation.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/AST/code_generation/AssemblyMemoryLocation.java
@@ -6,4 +6,37 @@ public class AssemblyMemoryLocation extends AssemblyExpression {
     private AssemblyRegister offsetRegister;
     private AssemblyRegister baseRegister;
 
+    public AssemblyMemoryLocation(AssemblyRegister baseRegister) {
+        this.baseRegister = baseRegister;
+    }
+
+    public AssemblyMemoryLocation(AssemblyRegister baseRegister,
+                                  AssemblyRegister offsetRegister) {
+        this.baseRegister = baseRegister;
+        this.offsetRegister = offsetRegister;
+    }
+
+    public AssemblyMemoryLocation(AssemblyRegister baseRegister,
+                                  AssemblyRegister offsetRegister,
+                                  long displacement) {
+        this.baseRegister = baseRegister;
+        this.offsetRegister = offsetRegister;
+        this.displacement = displacement;
+    }
+
+    public AssemblyMemoryLocation(AssemblyRegister baseRegister,
+                                  AssemblyRegister offsetRegister,
+                                  long displacement,
+                                  long scale) {
+        this.baseRegister = baseRegister;
+        this.offsetRegister = offsetRegister;
+        this.displacement = displacement;
+        this.scale = scale;
+    }
+
+    @Override
+    public String toString() {
+        //TODO: Somebody who know this syntax should do this
+        return null;
+    }
 }
