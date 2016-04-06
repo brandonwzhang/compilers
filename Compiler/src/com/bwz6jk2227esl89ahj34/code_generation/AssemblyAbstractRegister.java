d7fab950dc194266002a28037b9d2974d2498a73
Brandon Zhang <bwzhang@gmail.com>
2016-04-06 18:59:35 -0400

    Layed out some ground work for CodeGenerator

--- Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/AssemblyAbstractRegister.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/AssemblyAbstractRegister.java
@@ -1,9 +1,34 @@
 package com.bwz6jk2227esl89ahj34.code_generation;
 
+import com.bwz6jk2227esl89ahj34.ir.IRTemp;
+
+import java.util.HashMap;
+
 public class AssemblyAbstractRegister extends AssemblyExpression {
+    // Maintains the next id to be assigned to a new register
+    private static int curId = 0;
+    // Keeps track of id's that were assigned to given names
+    private static HashMap<String, Integer> nameIdMap = new HashMap<>();
+
+    // The id for this instance
     public int id;
 
-    public AssemblyAbstractRegister(int id) {
-        this.id = id;
+    public AssemblyAbstractRegister() {
+        id = curId++;
+    }
+
+    /**
+     * Returns a new id if the given temp's name has not been encountered before. Otherwise,
+     * return the id stored in the map.
+     */
+    public AssemblyAbstractRegister(IRTemp temp) {
+        String name = temp.name();
+        Integer registerId = nameIdMap.get(name);
+        if (registerId != null) {
+            id = registerId;
+            return;
+        }
+        id = curId++;
+        nameIdMap.put(name, id);
     }
 }
---
+++ Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/CodeGenerator.java
@@ -0,0 +1,38 @@
+package com.bwz6jk2227esl89ahj34.code_generation;
+
+import com.bwz6jk2227esl89ahj34.ir.IRCompUnit;
+import com.bwz6jk2227esl89ahj34.ir.IRFuncDecl;
+import com.bwz6jk2227esl89ahj34.ir.IRSeq;
+
+import java.util.List;
+
+public class CodeGenerator {
+    private TileContainer tileContainer = new TileContainer();
+
+    /**
+     * Add all instruction tiles to tileContainer
+     */
+    private void intialize() {
+
+    }
+
+    /**
+     * Generate abstract assembly code for a program
+     * @param root IRCompUnit of program
+     * @return
+     */
+    public List<AssemblyInstruction> generateAbstract(IRCompUnit root) {
+
+        return null;
+    }
+
+    /**
+     * Generate abstract assembly code for a single function
+     * @param func the IRFuncDecl to be translated to abstract assembly
+     * @return
+     */
+    private List<AssemblyInstruction> generateFunction(IRFuncDecl func) {
+
+        return null;
+    }
+}
