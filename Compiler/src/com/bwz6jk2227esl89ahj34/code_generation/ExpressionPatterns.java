50632da778cc7646f24ae27fb57929488db95813
Ji Hun Kim <jk2227@cornell.edu>
2016-04-06 22:16:16 -0400

    started putting in patterns

--- Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/ExpressionPatterns.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/ExpressionPatterns.java
@@ -3,4 +3,12 @@ package com.bwz6jk2227esl89ahj34.code_generation;
 import com.bwz6jk2227esl89ahj34.ir.*;
 
 public class ExpressionPatterns {
+    /*
+            Dummy nodes that represent tile patterns. Ignore values inside, such as 0.
+        null represents a generic pattern, for lack of better solution. Problems? email ahj34@cornell.edu.
+    */
+    IRNode const1 = new IRConst(0);
+    IRNode temp1 = new IRTemp("");
+    IRNode mem1 = new IRMem(null);
+
 }
--- Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/StatementPatterns.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/StatementPatterns.java
@@ -3,4 +3,14 @@ package com.bwz6jk2227esl89ahj34.code_generation;
 import com.bwz6jk2227esl89ahj34.ir.*;
 
 public class StatementPatterns {
+    /*
+            Dummy nodes that represent tile patterns. Ignore values inside, such as 0, or "".
+        All StatementPatterns have roots that are IRStmts.
+        null represents a generic pattern, for lack of better solution. Problems? email ahj34@cornell.edu.
+    */
+    IRStmt move1 = new IRMove(null, null);
+    IRStmt jump1 = new IRJump((IRExpr)null);
+    IRStmt label1 = new IRLabel("");
+
+
 }
