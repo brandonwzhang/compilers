45363052fc2a108ef640a5d609a1be8b1edcdca2
Ji Hun Kim <jk2227@cornell.edu>
2016-04-11 20:35:57 -0400

    setting up junit testing for pa5...

---
+++ Compiler/src/com/bwz6jk2227esl89ahj34/PA5Tests.java
@@ -0,0 +1,15 @@
+package com.bwz6jk2227esl89ahj34;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jihunkim on 4/11/16.
+ */
+public class PA5Tests {
+    @Test
+    public void testSomething() {
+        Assert.assertTrue(true);
+    }
+}
+
