3daeb1cd9fde4ce9bfb09c6d998853d4ae07f685
Ji Hun Kim <jk2227@cornell.edu>
2016-04-12 23:34:32 -0400

    lol

--- Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/TileContainer.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/code_generation/TileContainer.java
@@ -26,10 +26,10 @@ public class TileContainer {
         add(new StatementTile(StatementPatterns.return1, StatementCodeGenerators.return1));
         add(new StatementTile(StatementPatterns.cjump1, StatementCodeGenerators.cjump1));
 
-        add(new StatementTile(StatementPatterns.move3, StatementCodeGenerators.move3456));
-        add(new StatementTile(StatementPatterns.move4, StatementCodeGenerators.move3456));
-        add(new StatementTile(StatementPatterns.move5, StatementCodeGenerators.move3456));
-        add(new StatementTile(StatementPatterns.move6, StatementCodeGenerators.move3456));
+       // add(new StatementTile(StatementPatterns.move3, StatementCodeGenerators.move3456));
+       // add(new StatementTile(StatementPatterns.move4, StatementCodeGenerators.move3456));
+       // add(new StatementTile(StatementPatterns.move5, StatementCodeGenerators.move3456));
+       // add(new StatementTile(StatementPatterns.move6, StatementCodeGenerators.move3456));
     }
 
     public static boolean add(Tile tile) {
