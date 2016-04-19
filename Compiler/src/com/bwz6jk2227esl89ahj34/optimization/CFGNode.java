ba722bbb38ec205ca24821a0d64aec6e094d2535
Eric Lin <ericsuhelin@gmail.com>
2016-04-19 00:12:12 -0400

    filled out cfgnode

--- Compiler/src/com/bwz6jk2227esl89ahj34/optimization/CFGNode.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/optimization/CFGNode.java
@@ -1,4 +1,15 @@
 package com.bwz6jk2227esl89ahj34.optimization;
 
-public class CFGNode {
+import com.bwz6jk2227esl89ahj34.ir.IRNode;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class CFGNode<T, E> {
+    List<CFGNode> predecessors;
+    List<CFGNode> successors;
+
+    T data;
+    HashSet<E> in;
+    HashSet<E> out;
 }
