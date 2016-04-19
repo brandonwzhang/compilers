01401cb7dd46311b55100b3e660045b912eee1bf
Eric Lin <ericsuhelin@gmail.com>
2016-04-19 17:23:28 -0400

    small class changes

--- Compiler/src/com/bwz6jk2227esl89ahj34/optimization/DataflowAnalysis.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/optimization/DataflowAnalysis.java
@@ -1,4 +1,9 @@
 package com.bwz6jk2227esl89ahj34.optimization;
 
+import java.util.Set;
+
 public abstract class DataflowAnalysis {
+
+
+    public abstract LatticeElement meet(Set<LatticeElement> elements);
 }
--- Compiler/src/com/bwz6jk2227esl89ahj34/optimization/LatticeElement.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/optimization/LatticeElement.java
@@ -1,4 +1,7 @@
 package com.bwz6jk2227esl89ahj34.optimization;
 
+import java.util.Set;
+
 public abstract class LatticeElement {
+
 }
