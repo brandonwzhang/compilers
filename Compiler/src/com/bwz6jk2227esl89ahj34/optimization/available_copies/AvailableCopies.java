247806687abd536d5d43e69d35a5cd454111b964
Ji Hun Kim <jk2227@cornell.edu>
2016-04-21 01:00:35 -0400

    pushing changes for available copies

--- Compiler/src/com/bwz6jk2227esl89ahj34/optimization/available_copies/AvailableCopies.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/optimization/available_copies/AvailableCopies.java
@@ -2,9 +2,9 @@ package com.bwz6jk2227esl89ahj34.optimization.available_copies;
 
 import com.bwz6jk2227esl89ahj34.ir.IRStmt;
 import com.bwz6jk2227esl89ahj34.optimization.CFGNode;
+import com.bwz6jk2227esl89ahj34.optimization.CFGNodeIR;
 import com.bwz6jk2227esl89ahj34.optimization.DataflowAnalysis;
 import com.bwz6jk2227esl89ahj34.optimization.LatticeElement;
-import com.bwz6jk2227esl89ahj34.optimization.CFGNode.NodeType;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,30 +16,44 @@ public class AvailableCopies extends DataflowAnalysis {
 
      public void transfer(CFGNode node) {
          // perform the analysis on the IR level
-         assert node.getNodeType() == NodeType.IR;
+         assert node instanceof CFGNodeIR;
 
          // unwrap the IR stmt contained by node
-         IRStmt stmt = node.getIrstmt();
+         CFGNodeIR castedNode = (CFGNodeIR) node;
+         IRStmt stmt = castedNode.getStatement();
 
          // assumption: out of predecessor is filled in
          AvailableCopiesSet in;
          Set<CFGNode> predecessors = node.getPredecessors();
-         if (predecessors.size() > 1) { 
+         if (predecessors.size() > 1) { // if multiple predecessors...
              Set<LatticeElement> preds = new HashSet<>();
              for (CFGNode predNode : predecessors) {
                  preds.add(predNode.getOut());
-             }
-             in = meet(preds);
-         } else {
+             }                 // we meet the out of the predecessors
+             in = meet(preds); // and set it as the in
+         } else { // if single predecessor, set the out as the in
              in = (AvailableCopiesSet) predecessors.iterator().next().getOut();
          }
 
-         node.setIn(in);
-         node.setOut(out(node));
+         node.setIn(in); // set in of node to in
+         node.setOut(out(node));  // set out of node to result of out method
 
      }
 
+     // meet is an intersection for AvailableCopies
+     // precondition: elements are out of the predecessors
+     // postcondition: in[n] = for all predecessors of n n',
+     //                        return intersection of out[n']
      public AvailableCopiesSet meet(Set<LatticeElement> elements) {
+         AvailableCopiesSet accumulator = null;
+         for(LatticeElement pred : elements) {
+             if (accumulator == null) {
+                 accumulator = (AvailableCopiesSet) pred;
+             } else {
+                 AvailableCopiesSet castedPred = (AvailableCopiesSet) pred;
+
+             }
+         }
          return null;
      }
 
--- Compiler/src/com/bwz6jk2227esl89ahj34/optimization/available_copies/AvailableCopiesSet.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/optimization/available_copies/AvailableCopiesSet.java
@@ -1,10 +1,13 @@
 package com.bwz6jk2227esl89ahj34.optimization.available_copies;
 
 import com.bwz6jk2227esl89ahj34.ir.IRTemp;
+import com.bwz6jk2227esl89ahj34.optimization.LatticeBottom;
 import com.bwz6jk2227esl89ahj34.optimization.LatticeElement;
+import com.bwz6jk2227esl89ahj34.optimization.LatticeTop;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -14,4 +17,35 @@ import java.util.Map;
 @AllArgsConstructor
 public class AvailableCopiesSet extends LatticeElement {
     private Map<IRTemp, IRTemp> map;
+
+    public boolean equals(LatticeElement set) {
+        if (!(set instanceof AvailableCopiesSet)) {
+            return false;
+        } else {
+            return map.equals(((AvailableCopiesSet)set).getMap());
+        }
+    }
+
+    public void intersect(LatticeElement set) {
+        if (set instanceof LatticeTop) {
+            // then map stays the same
+        } else if (set instanceof LatticeBottom) {
+            // then we empty out map
+            map = new HashMap<>();
+        } else if (set instanceof AvailableCopiesSet) {
+            // then we take information that is common in both
+            AvailableCopiesSet castedSet = (AvailableCopiesSet) set;
+            Map<IRTemp, IRTemp> castedSetMap = castedSet.getMap();
+            Map<IRTemp, IRTemp> newMap = new HashMap<>();
+            for (IRTemp key : map.keySet()) {
+                if (castedSetMap.containsKey(key) &&
+                        castedSetMap.get(key).name().equals(map.get(key).name())) {
+                    newMap.put(key, map.get(key));
+                }
+            }
+            map = newMap;
+        }
+    }
+
+
 }
