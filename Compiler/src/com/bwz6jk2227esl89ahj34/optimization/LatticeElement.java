067d6087ff2e11e05c876a0cea3b83b0d1f12941
Brandon Zhang <bwzhang@gmail.com>
2016-04-21 00:57:28 -0400

    Added specs for LatticeElement functions

--- Compiler/src/com/bwz6jk2227esl89ahj34/optimization/LatticeElement.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/optimization/LatticeElement.java
@@ -1,6 +1,13 @@
 package com.bwz6jk2227esl89ahj34.optimization;
 
 public abstract class LatticeElement {
+    /**
+     * Return true iff this element is equal to the given element by the partial order
+     */
     public abstract boolean equals(LatticeElement element);
+
+    /**
+     * Returns a deep copy of this LatticeElement
+     */
     public abstract LatticeElement copy();
 }
