5a3f11e42e05fea8add170ec9e60537126fd428e
Ji Hun Kim <jk2227@cornell.edu>
2016-04-25 02:57:14 -0400

    I had to make the graph public in data flow analysis to override the fixpoint method

--- Compiler/src/com/bwz6jk2227esl89ahj34/dataflow_analysis/DataflowAnalysis.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/dataflow_analysis/DataflowAnalysis.java
@@ -17,7 +17,7 @@ public abstract class DataflowAnalysis {
         FORWARD, BACKWARD
     }
 
-    private CFG graph;
+    public CFG graph;
     protected LatticeElement top;
     protected LatticeElement bottom;
 
