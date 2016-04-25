fa20e87fb23b970dd069186e2f04d72d6663fa1e
Ji Hun Kim <jk2227@cornell.edu>
2016-04-25 03:29:43 -0400

    fixed assertion error for Value

--- Compiler/src/com/bwz6jk2227esl89ahj34/dataflow_analysis/conditional_constant_propagation/Value.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/dataflow_analysis/conditional_constant_propagation/Value.java
@@ -1,7 +1,9 @@
 package com.bwz6jk2227esl89ahj34.dataflow_analysis.conditional_constant_propagation;
 
 
+import com.bwz6jk2227esl89ahj34.dataflow_analysis.LatticeBottom;
 import com.bwz6jk2227esl89ahj34.dataflow_analysis.LatticeElement;
+import com.bwz6jk2227esl89ahj34.dataflow_analysis.LatticeTop;
 import com.bwz6jk2227esl89ahj34.ir.IRConst;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +16,9 @@ public class Value extends LatticeElement {
 
     @Override
     public boolean equals(Object o) {
+        if (o instanceof LatticeTop || o instanceof LatticeBottom) {
+            return false;
+        }
         assert o instanceof Value;
         Value castedO = (Value)o;
         return this.getValue().value() == castedO.getValue().value();
