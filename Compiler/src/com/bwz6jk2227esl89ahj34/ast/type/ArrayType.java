bb9e8fffa92d7ee0a14ec56a16d53ba9c054e563
Brandon Zhang <bwzhang@gmail.com>
2016-05-06 17:48:20 -0400

    Partial refactor of TypeCheckVisitor

--- Compiler/src/com/bwz6jk2227esl89ahj34/ast/type/ArrayType.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/ast/type/ArrayType.java
@@ -1,15 +1,16 @@
 package com.bwz6jk2227esl89ahj34.ast.type;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class ArrayType extends VariableType {
     private BaseType baseType;
     private Integer numBrackets;
 
-    public ArrayType(BaseType t) {
-        this.baseType = t;
-        numBrackets = 0;
-    }
-
     public ArrayType(BaseType t, Integer numBrackets) {
+        assert numBrackets > 0;
         this.baseType = t;
         this.numBrackets = numBrackets;
     }
