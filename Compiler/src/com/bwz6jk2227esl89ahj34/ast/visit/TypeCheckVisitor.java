2e9d3bb4f6310d046c64775a9d5726ee7f83a48e
Brandon Zhang <bwzhang@gmail.com>
2016-05-06 22:06:25 -0400

    Removed useless variable

--- Compiler/src/com/bwz6jk2227esl89ahj34/ast/visit/TypeCheckVisitor.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/ast/visit/TypeCheckVisitor.java
@@ -14,8 +14,6 @@ public class TypeCheckVisitor implements NodeVisitor {
     // Maintain a stack of contexts (identifier -> type map) for scoping
     private Stack<Context> contexts;
 
-    private Context lastPoppedContext;
-
     // Local variable that stores current function in scope with a stack, used for return statements
     private FunctionType currentFunctionType;
 
@@ -278,7 +276,7 @@ public class TypeCheckVisitor implements NodeVisitor {
             Type lastType = blockList.get(size - 1).getType();
             node.setType(lastType);
         }
-        lastPoppedContext = contexts.pop();
+        contexts.pop();
     }
 
     /**
