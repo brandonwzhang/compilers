661fc2191c9b1cb85dec3be47338bc9fd414a51a
Brandon Zhang <bwzhang@gmail.com>
2016-05-10 19:59:57 -0400

    FunctionCall is not Assignable

--- Compiler/src/com/bwz6jk2227esl89ahj34/ast/FunctionCall.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/ast/FunctionCall.java
@@ -9,7 +9,7 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 @EqualsAndHashCode(callSuper=false)
-public class FunctionCall extends Expression implements Assignable {
+public class FunctionCall extends Expression {
     private Identifier identifier;
     private List<Expression> arguments;
 
