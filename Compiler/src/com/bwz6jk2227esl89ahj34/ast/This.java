07c5ff79f670b605364030ce21545126cd08598c
Ji Hun Kim <jk2227@cornell.edu>
2016-05-13 12:54:46 -0400

    woops

---
+++ Compiler/src/com/bwz6jk2227esl89ahj34/ast/This.java
@@ -0,0 +1,12 @@
+package com.bwz6jk2227esl89ahj34.ast;
+
+import com.bwz6jk2227esl89ahj34.ast.visit.NodeVisitor;
+
+/**
+ * Created by jihunkim on 5/12/16.
+ */
+public class This extends Expression {
+    public void accept(NodeVisitor v){
+        v.visit(this);
+    }
+}
