f7569e33d06d25ee15d11ef926479a23e278633f
Ji Hun Kim <jk2227@cornell.edu>
2016-05-13 22:58:42 -0400

    getting rid of lombok

--- Compiler/src/com/bwz6jk2227esl89ahj34/ast/type/Context.java
+++ Compiler/src/com/bwz6jk2227esl89ahj34/ast/type/Context.java
@@ -1,21 +1,33 @@
 package com.bwz6jk2227esl89ahj34.ast.type;
 
 import com.bwz6jk2227esl89ahj34.ast.Identifier;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
-import java.util.LinkedHashMap;
-
-@AllArgsConstructor
-@Data
-@EqualsAndHashCode(callSuper=false)
 public class Context {
     VariableContext variableContext;
     FunctionContext functionContext;
 
     public Context(Context c) {
-        this(c.getVariableContext(), c.getFunctionContext());
+        variableContext = new VariableContext();
+        functionContext = new FunctionContext();
+        for (Identifier key : c.getVariableContext().keySet()) {
+            this.variableContext.put(key, c.getVariableContext().get(key));
+        }
+        for (Identifier key : c.getFunctionContext().keySet()) {
+            this.functionContext.put(key, c.getFunctionContext().get(key));
+        }
+    }
+
+    public Context(VariableContext variableContext, FunctionContext functionContext) {
+        this.variableContext = variableContext;
+        this.functionContext = functionContext;
+    }
+
+    public VariableContext getVariableContext() {
+        return this.variableContext;
+    }
+
+    public FunctionContext getFunctionContext() {
+        return this.functionContext;
     }
 
     public void put(Identifier identifier, Type type) {
