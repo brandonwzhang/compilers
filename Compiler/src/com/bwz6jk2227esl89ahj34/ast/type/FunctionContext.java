11bd8013332c752c4b70fa1d1ab0cac522a8d0a0
Ji Hun Kim <jk2227@cornell.edu>
2016-05-13 21:50:32 -0400

    adding different contexts

---
+++ Compiler/src/com/bwz6jk2227esl89ahj34/ast/type/FunctionContext.java
@@ -0,0 +1,16 @@
+package com.bwz6jk2227esl89ahj34.ast.type;
+
+import com.bwz6jk2227esl89ahj34.ast.Identifier;
+
+import java.util.LinkedHashMap;
+
+public class FunctionContext extends LinkedHashMap<Identifier, Type> {
+
+    public FunctionContext() {
+        super();
+    }
+
+    public FunctionContext(Context c) {
+        super(c);
+    }
+}
